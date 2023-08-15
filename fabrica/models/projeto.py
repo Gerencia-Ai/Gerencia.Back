6cb271348705477cefc589b4066be59e0f27f229
labs <gustavodep.gorges@gmail.com>
2023-08-15 09:28:15 -0300

    começando a parte de segurança

--- fabrica/models/projeto.py
+++ fabrica/models/projeto.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import Group
 from usuario.models import Usuario
 
 
@@ -7,6 +8,7 @@ class Projeto(models.Model):
     descricao = models.CharField(max_length=2500)
     data = models.DateTimeField(auto_now_add=True)
     imagem = models.ImageField(upload_to='images/', null=True, blank=True)
+    grupo_id = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
     
     
     def __str__(self):
