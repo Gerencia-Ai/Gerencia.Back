90ae72f877d7c8c32f93691a268286c144897d12
GustavodePaulaGorges <Gustavodep.gorges@gmail.com>
2023-09-04 20:06:03 -0300

    projeto img (without migrations)

--- fabrica/models/projeto.py
+++ fabrica/models/projeto.py
@@ -1,5 +1,6 @@
 from django.db import models
 from usuario.models import Usuario
+from uploader.models import Image
 
 
 class Projeto(models.Model):
@@ -9,6 +10,14 @@ class Projeto(models.Model):
     imagem = models.ImageField(upload_to='images/', null=True, blank=True)  
     professor = models.ForeignKey(Usuario, related_name='professor', on_delete=models.PROTECT) 
     alunos = models.ManyToManyField(Usuario, related_name='alunos') 
+    capa = models.ForeignKey(
+        Image,
+        related_name="+",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        default=None,
+    )
     
     def __str__(self):
         return self.nome
