c75a5c80d7d12058f07a850714d168c0ab49ee40
labs <gustavodep.gorges@gmail.com>
2023-09-05 16:55:33 -0300

    arrumado model foto de perfil

--- usuario/models.py
+++ usuario/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from uploader.models import Image
 
 from .managers import CustomUserManager
 
@@ -13,7 +14,7 @@ class Usuario(AbstractUser):
     data_nascimento = models.DateField(
         _("Birth Date"), auto_now=False, auto_now_add=False, blank=True, null=True
     )
-    foto = models.ImageField(upload_to="images/", null=True, blank=True,  related_name="foto", default="images/default.png")
+    foto = models.ManyToManyField(Image, related_name="+", blank=True)
     
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
