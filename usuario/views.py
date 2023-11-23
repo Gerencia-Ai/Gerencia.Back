521188154020ff8eb58148749b7896c86755f050
GustavodePaulaGorges <Gustavodep.gorges@gmail.com>
2023-11-23 14:07:59 -0300

    dumb errors fixed?

--- usuario/views.py
+++ usuario/views.py
@@ -3,9 +3,6 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Usuario
 from .serializers import UsuarioSerializer
 
-from .models import TipoUsuario
-from .serializers import TipoUsuarioSerializer
-
 
 class UsuarioViewSet(ModelViewSet):
     queryset = Usuario.objects.all()
